09425676b5b605b133598278582810f7a0368496
Vincent Yao <qiuchen_yao@126.com>
2015-01-18 22:57:26 +0800

    fix timestamp issue

--- web/src/main/sql/schema/init-huntering-schema.sql
+++ web/src/main/sql/schema/init-huntering-schema.sql
@@ -11,7 +11,7 @@ create table `account`(
   `deleted`   bool,
   `active`   bool,
   `create_date` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `update_date` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_date` timestamp,
   constraint `pk_account` primary key(`id`)
 ) charset=utf8 ENGINE=InnoDB;;
 alter table `account` auto_increment=1000;;
@@ -25,7 +25,7 @@ create table `email`(
   `main`   bool,
   `active`   bool,
   `create_date` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `update_date` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_date` timestamp,
   constraint `pk_email` primary key(`id`),
   constraint `unique_email` unique(`email`),
   index `idx_email_main` (`main`)
@@ -39,7 +39,7 @@ create table `invitation_code`(
   `code`  varchar(100),
   `used`   bool,
   `create_date` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `update_date` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_date` timestamp,
   constraint `pk_invitation_code` primary key(`id`),
   constraint `unique_invitation_code` unique(`code`),
   index `idx_invitation_code_used` (`used`)
