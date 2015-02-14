9fd2522b6193fe409f9e0f1b85a43ce50afdf5fc
Bell <qiulh@mail.ustc.edu.cn>
2015-02-14 21:49:55 +0800

    Fix FireFox feedback page error

--- web/src/main/webapp/WEB-INF/static/js/feedback.js
+++ web/src/main/webapp/WEB-INF/static/js/feedback.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	$(".feedback_desb").each(function(index, el){
 		$(el).click(function(){
 			console.log(el);
-			var parText = el.parentElement.firstElementChild.innerText;
+			//var parText = el.parentElement.firstElementChild.innerText;
+			var parText = $(el).parent().find("label").text();
 			var currentDetail = $("#feedback_detail").val();
 			if(currentDetail != "") {
 				currentDetail = currentDetail + ", ";
